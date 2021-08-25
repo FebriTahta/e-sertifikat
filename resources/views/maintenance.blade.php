ffc4452c3cf41a21175262af36a770d94164312d
FebriTahta <febrirtah@gmail.com>
2021-08-24 23:51:51 -0700

    keterangan

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -1,18 +1,26 @@
 <!doctype html>
 <title>Site Maintenance</title>
-<style>
-  body { text-align: center; padding: 150px; }
-  h1 { font-size: 50px; }
-  body { font: 20px Helvetica, sans-serif; color: #333; }
-  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
-  a { color: #dc8100; text-decoration: none; }
-  a:hover { color: #333; text-decoration: none; }
-</style>
+<head>
+	<meta charset="utf-8">
+	<meta property="og:title" content="Download E-Certificate"/>
+	<meta property="og:description" content="Selamat datang para pecinta Al-Qur'an, terimakasih telah ikut serta
+     dalam diklat tilawati. download e-certificate anda disini"/>
+	<meta property="og:image" itemprop="image" content="{{ asset('assets/images/tumb.jpeg') }}">
+	<style>
+	  body { text-align: center; padding: 150px; }
+	  h1 { font-size: 50px; }
+	  body { font: 20px Helvetica, sans-serif; color: #333; }
+	  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+	  a { color: #dc8100; text-decoration: none; }
+	  a:hover { color: #333; text-decoration: none; }
+	</style>
+	
+</head>
 
 <article>
     <h1>Sistem sedang dalam perbaikan</h1>
     <div>
-        <p>Saat ini sistem yang berjalan sedang dalam perbaikan dan akan kami buka sesegera mungkin</p>
+        <p>Server dalam proses pemindahan dari US ke Indonesia untuk optimasi kecepatan akses data. Apabila proses sudah selesai akan segera kami infokan</p>
 		<p>Hormat kami</p>
         <p>&mdash; IT & Administration Tilawati Pusat</p>
     </div>
