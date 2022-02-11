7d570bfabaa77727dc19419b8b528f8b0f6a86d5
FebriTahta <febrirtah@gmail.com>
2022-02-11 20:00:44 +0700

    oke

--- resources/views/e_sertifikat/index.blade.php
+++ resources/views/e_sertifikat/index.blade.php
@@ -30,7 +30,7 @@
 					<ul class="accordion-box">	
 						<!--Block-->
 						<li class="accordion block"> <?php date_default_timezone_set('Asia/Jakarta'); $date=$item->tgl_awal;?>
-							<div class="acc-btn" style="text-transform: capitalize"><div class="icon-outer"><span class="icon icon-plus flaticon-plus-symbol"></span> <span class="icon icon-minus flaticon-substract"></span></div><span style="text-transform: capitalize"> {{ Carbon\Carbon::parse($date)->isoFormat('dddd, D MMMM Y') }} -</span> {{ strtolower($item->program->id) }}</div>
+							<div class="acc-btn" style="text-transform: capitalize"><div class="icon-outer"><span class="icon icon-plus flaticon-plus-symbol"></span> <span class="icon icon-minus flaticon-substract"></span></div><span style="text-transform: capitalize"> {{ Carbon\Carbon::parse($date)->isoFormat('dddd, D MMMM Y') }} -</span> {{ strtolower($item->program) }}</div>
 							<div class="acc-content">
 								<div class="content">
 									<div class="text">
