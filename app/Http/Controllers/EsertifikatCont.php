b293e45ba459a14a884fe6acbc94a365f12be90e
FebriTahta <febrirtah@gmail.com>
2022-04-10 20:54:47 -0700

    oke

--- app/Http/Controllers/EsertifikatCont.php
+++ app/Http/Controllers/EsertifikatCont.php
@@ -19,7 +19,7 @@ class EsertifikatCont extends Controller
 
     public function list($slug,$id)
     {
-        $diklat     = Induksertifikat::where('id',$id)->first();
+        $diklat     = Induksertifikat::where('slug',$slug)->where('id',$id)->first();
         if ($diklat->tgl_akhir !== null) {
             # code...
             $tgl = Carbon::parse($diklat->tgl_awal)->isoFormat('dddd, D MMMM Y').' - '.Carbon::parse($diklat->tgl_akhir)->isoFormat('dddd, D MMMM Y');
